dfdc1907ffee81fc963e7986b447fa541dc912bf
Daniel Keyes <dkeyes6@gatech.edu>
2015-01-12 13:42:42 -0500

    Oops, missed a couple files.

---
+++ Battlecode/teams/simpleframework/Util.java
@@ -0,0 +1,42 @@
+package simpleframework;
+
+import java.util.LinkedList;
+import java.util.Random;
+
+import battlecode.common.Direction;
+
+public class Util {
+
+	// these are all the directions excluding OMNI and NONE
+	public static final Direction[] actualDirections = { Direction.NORTH, Direction.NORTH_EAST, Direction.EAST,
+			Direction.SOUTH_EAST, Direction.SOUTH, Direction.SOUTH_WEST, Direction.WEST, Direction.NORTH_WEST, };
+
+	// precondition: 0 <= i <= 12
+	public static int factorial(int i) {
+		return f[i];
+	}
+
+	private static int[] f = { 1, 1, 2, 6, 24, 120, 720, 5040, 40320, 362880, 3628800, 39916800, 479001600 };
+
+	public static Direction[] getRandomDirectionOrdering(Random gen) {
+		// create a random permutation
+		LinkedList<Direction> dirsLeft = new LinkedList<Direction>();
+
+		for (int i = 0; i < actualDirections.length; i++) {
+			dirsLeft.add(actualDirections[i]);
+		}
+
+		Direction[] ans = new Direction[dirsLeft.size()];
+		int index = gen.nextInt(Util.factorial(8));
+		for (int i = dirsLeft.size(); i >= 1; i--) {
+			int selected = index % i;
+			index /= i;
+			ans[i - 1] = dirsLeft.remove(selected);
+		}
+		return ans;
+	}
+
+	public static Direction getRandomDirection(Random gen) {
+		return actualDirections[gen.nextInt(actualDirections.length)];
+	}
+}
