a78c50bc897a1837276974b806408fdf6410e05e
Daniel Keyes <dkeyes6@gatech.edu>
2015-01-26 12:46:47 -0500

    Nevermind, group hugs are causing too much congestion.

--- Battlecode/teams/dronerush/BaseRobotHandler.java
+++ Battlecode/teams/dronerush/BaseRobotHandler.java
@@ -801,9 +801,6 @@ public abstract class BaseRobotHandler {
 			}
 		}
 
-		private final int HUG_DISTANCE = 2;
-		private final int PERSONAL_SPACE_DISTANCE = 9;
-
 		@Override
 		public boolean run() throws GameActionException {
 			MapLocation towerInPeril = BroadcastInterface.getTowerInPeril(rc);
@@ -833,14 +830,7 @@ public abstract class BaseRobotHandler {
 
 			boolean result = curAction.run();
 
-			int threshholdDist;
-			// if we're responding to a distress call, make sure to give the tower a reassuring hug
-			if (isRespondingToDistressSignal) {
-				threshholdDist = HUG_DISTANCE;
-			} else {
-				threshholdDist = PERSONAL_SPACE_DISTANCE;
-			}
-			if (rc.getLocation().distanceSquaredTo(target) <= threshholdDist) {
+			if (rc.getLocation().distanceSquaredTo(target) <= 8) {
 				isTravelingToTower = false;
 			}
 
